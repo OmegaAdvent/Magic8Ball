fffaf39746d6c3d123b2c8c5767c427dad8a8a05
OmegaAdvent <113447989+OmegaAdvent@users.noreply.github.com>
2022-09-13 09:29:39 -0500

    Add files via upload
    This is an example of a magic 8 ball in java

---
+++ Magic8Ball.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Magic8Ball
+{
+    public static void main(String[] args ){
+        System.out.println("This is the Magic 8 Ball");
+
+        System.out.println("Ask the Magic 8 Ball a question.");
+
+        Scanner input = new Scanner (System.in);
+
+        String number = input.next();
+
+        if (number = 1);
+
+        { System.out.println("Good lucky today");
+        }
+        if  (number = 2);
+
+        { System.out.println("Things could get better");
+        }
+        if (number = 3);
+
+        {  System.out.println("Try again next time");
+        }
+        if (number = 4);
+
+        {  System.out.println("Things are looking possible");
+        }  if (number = 5);
+
+        {  System.out.println("I wouldn't get your hopes up");
+        }  if (number = 6);
+
+        { System.out.println("Outcome looks good");
+        }  if (number = 7);
+
+        { System.out.println("All signs say yes");
+        }  if (number = 8);
+            System.out.println("All signs say no");
+    }
+
+
+
+    }
+
+
+
